e8fc4dda1c5559691875366df98314f3d5f7081c
tom_pro <tom_pro@yang.com>
2020-02-24 17:48:41 +0800

    update main

--- src/com/yang/Main.java
+++ src/com/yang/Main.java
@@ -5,6 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello github");
         System.out.println("hello github from github server");
-        System.out.println("local update");       
+        System.out.println("local server");       
 	}
 }
