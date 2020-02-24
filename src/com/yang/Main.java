27d98e17a6770c282b7e820eff0e5f27a7ddfda4
yang201806 <39898535+yang201806@users.noreply.github.com>
2020-02-24 17:45:04 +0800

    server update main.java
    server update main.java

--- src/com/yang/Main.java
+++ src/com/yang/Main.java
@@ -5,5 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello github");
                 System.out.println("hello github from github server");
+System.out.println("server udate");
 	}
 }
